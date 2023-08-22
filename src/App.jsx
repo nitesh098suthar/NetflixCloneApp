d3c1fa8dfe5c1da2280d1e65859ad38a89876987
nitesh098suthar <nitesh098suthar@gmail.com>
2023-08-22 12:41:58 +0530

    Uploaded

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Home/Header.jsx'
+// import axios from "axios";
+
+const App = () => {
+  return (
+    <>
+    <Router>
+
+      <Header />
+
+
+      <Routes>
+        <Route />
+      </Routes>
+    
+    </Router> 
+    
+   
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../../assets/Logo/netflix logo.png'
+
+const Header = () => {
+  return (
+    <>
+     <div className=' flex justify-between items-center p-4 min-h-screen bg-black/10 bg-cover bg-center w-100 ' style={{background: url("images/background.jpg")}} >
+        <div>
+            <img src={logo} alt="image" className='w-[150px] ml-10 '/>
+        </div>
+        <div className=''>
+           <input type="button" value="English" className='border-2 rounded-md  px-4 mx-8 py-1 text-sm font-semibold cursor-pointer' />
+           <input type="button" value="Sign In" className='bg-red-600 px-2 py-1 rounded-md font-semibold text-white text-sm tracking-wide cursor-pointer mr-10' />
+        </div>        
+
+     </div>
+    </>
+  )
+}
+
+export default Header
