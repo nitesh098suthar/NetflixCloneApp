af80718e75e3a3f99bdc3bab197970c9f08db369
nitesh098suthar <nitesh098suthar@gmail.com>
2023-08-22 12:53:43 +0530

    Background Images added

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -4,7 +4,8 @@ import logo from '../../assets/Logo/netflix logo.png'
 const Header = () => {
   return (
     <>
-     <div className=' flex justify-between items-center p-4 min-h-screen bg-black/10 bg-cover bg-center w-100 ' style={{background: url("images/background.jpg")}} >
+     <div className='min-h-screen bg-black' style="background-image: url('https://www.pexels.com/photo/woman-standing-in-front-of-sea-1440399/')">
+     <div className=' flex justify-between items-center p-4'  >
         <div>
             <img src={logo} alt="image" className='w-[150px] ml-10 '/>
         </div>
@@ -13,6 +14,7 @@ const Header = () => {
            <input type="button" value="Sign In" className='bg-red-600 px-2 py-1 rounded-md font-semibold text-white text-sm tracking-wide cursor-pointer mr-10' />
         </div>        
 
+     </div>
      </div>
     </>
   )
